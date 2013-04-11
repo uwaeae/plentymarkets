10f2f375993660faca57f5c288fee154055fbbf0
uwaeae <florian.engler@gmx.de>
2013-04-11 12:53:35 +0200

    Kassen Update

--- web/js/checkouthistory.js
+++ web/js/checkouthistory.js
@@ -4,13 +4,44 @@ $(document).ready(function(){
             //console.log(date);
             var URL=$('#datepicker').data('url');
             URL = URL.replace("now",date);
-            $('#result').empty().load(URL+' #result');
+            $('#result').empty().load(URL+' #result',function(){
+                init();
+            });
             var URL2=$('#report').attr('href');
             URL2 = URL2.split('/');
             URL2.pop();
             URL2.push(date);
             $('#report').attr('href',URL2.join('/'));
+
         },
         dateFormat: "dd-mm-yy"
     });
+    init();
+
+
+
 });
+
+function init(){
+    $('.article_items').hide();
+
+    $('.article_button').click(function(){
+        $('article_button').show();
+        $(this).hide();
+        $('.article_items').slideUp();
+
+        $(this).parent().find('.article_items').slideToggle()
+
+    });
+    $('.bon').click(function(){
+        $.post($(this).attr( 'href' ),function(data) {
+
+            w=window.open();
+            w.document.write(data);
+            w.print();
+            w.close();
+        });
+    })
+
+
+}
